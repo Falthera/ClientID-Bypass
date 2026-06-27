bb29434de56c6dc0bf6b81f364c9c10c51fd6f58
Falthera <Falthera@users.noreply.github.com>
2026-06-27 16:47:39 +1000

    Use net.minecraft.util.Identifier for MC 1.21.11 Yarn mappings

--- src/main/java/com/novinitygames/clientid/client/ClientIDBypassMod.java
+++ src/main/java/com/novinitygames/clientid/client/ClientIDBypassMod.java
@@ -2,7 +2,7 @@ package com.novinitygames.clientid.client;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking;
-import net.minecraft.class_2960;
+import net.minecraft.util.Identifier;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -14,9 +14,9 @@ public class ClientIDBypassMod implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        ClientLoginNetworking.registerGlobalReceiver(new class_2960(MODCHECK_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createModCheckPacket()));
-        ClientLoginNetworking.registerGlobalReceiver(new class_2960(MODLIST_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createModListPacket()));
-        ClientLoginNetworking.registerGlobalReceiver(new class_2960(PACKLIST_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createPackListPacket()));
-        ClientLoginNetworking.registerGlobalReceiver(new class_2960(CLIENTVERSION_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createClientVersionPacket()));
+        ClientLoginNetworking.registerGlobalReceiver(Identifier.of(MODCHECK_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createModCheckPacket()));
+        ClientLoginNetworking.registerGlobalReceiver(Identifier.of(MODLIST_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createModListPacket()));
+        ClientLoginNetworking.registerGlobalReceiver(Identifier.of(PACKLIST_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createPackListPacket()));
+        ClientLoginNetworking.registerGlobalReceiver(Identifier.of(CLIENTVERSION_CHANNEL), (client, handler, buf, callbacks) -> CompletableFuture.completedFuture(ClientIDNetworkHandler.createClientVersionPacket()));
     }
 }
